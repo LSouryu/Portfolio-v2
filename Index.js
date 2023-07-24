8e4287263adb0f22a58423dce79a110d6ecac014
LSouryu <n.allary@hotmail.com>
2023-07-24 16:26:01 +0200

    cummit

--- Index.js
+++ Index.js
@@ -1,72 +1,80 @@
-function AffAcc(){
-    let accueil1 = document.getElementById("Section-Accueil-1");
-    let accueil2 = document.getElementById("Section-Accueil-2");
-    let APropos = document.getElementById("Section-A-Propos");
-    let CompGen = document.getElementById("Section-Compétence-Ensemble");
-    let Comp1 = document.querySelector(".Div-Comp-01");
-    let Comp2 = document.querySelector(".Div-Comp-02");
-    let Proj = document.getElementById("Section-Project");
-    let ProjGen1 = document.getElementById("Div-Project-Gen-1");
-    accueil1.style.display = "flex";
-    accueil2.style.display = "flex";
-    APropos.style.display = "none";
-    CompGen.style.display = "none";
-    Comp1.style.display = "none";
-    Comp2.style.display = "none";
-    Proj.style.display = "none";
-    ProjGen1.style.display = "none";
+function AffAcc() {
+  let accueil1 = document.getElementById("Section-Accueil-1");
+  let accueil2 = document.getElementById("Section-Accueil-2");
+  let APropos = document.getElementById("Section-A-Propos");
+  let CompGen = document.getElementById("Section-Compétence-Ensemble");
+  let Comp1 = document.querySelector(".Div-Comp-01");
+  let Comp2 = document.querySelector(".Div-Comp-02");
+  let Proj = document.getElementById("Section-Project");
+  let ProjGen1 = document.getElementById("Div-Project-Gen-1");
+  accueil1.style.display = "flex";
+  accueil2.style.display = "flex";
+  APropos.style.display = "none";
+  CompGen.style.display = "none";
+  Comp1.style.display = "none";
+  Comp2.style.display = "none";
+  Proj.style.display = "none";
+  ProjGen1.style.display = "none";
 }
-function AffProp(){
-    let accueil1 = document.getElementById("Section-Accueil-1");
-    let accueil2 = document.getElementById("Section-Accueil-2");
-    let APropos = document.getElementById("Section-A-Propos");
-    let CompGen = document.getElementById("Section-Compétence-Ensemble");
-    let Comp1 = document.querySelector(".Div-Comp-01");
-    let Comp2 = document.querySelector(".Div-Comp-02");
-    let Proj = document.getElementById("Section-Project");
-    let ProjGen1 = document.getElementById("Div-Project-Gen-1");
-    accueil1.style.display = "none";
-    accueil2.style.display = "none";
-    APropos.style.display = "flex";
-    CompGen.style.display = "none";
-    Comp1.style.display = "none";
-    Comp2.style.display = "none";
-    Proj.style.display = "none";
-    ProjGen1.style.display = "none";
+function AffProp() {
+  let accueil1 = document.getElementById("Section-Accueil-1");
+  let accueil2 = document.getElementById("Section-Accueil-2");
+  let APropos = document.getElementById("Section-A-Propos");
+  let CompGen = document.getElementById("Section-Compétence-Ensemble");
+  let Comp1 = document.querySelector(".Div-Comp-01");
+  let Comp2 = document.querySelector(".Div-Comp-02");
+  let Proj = document.getElementById("Section-Project");
+  let ProjGen1 = document.getElementById("Div-Project-Gen-1");
+  accueil1.style.display = "none";
+  accueil2.style.display = "none";
+  APropos.style.display = "flex";
+  CompGen.style.display = "none";
+  Comp1.style.display = "none";
+  Comp2.style.display = "none";
+  Proj.style.display = "none";
+  ProjGen1.style.display = "none";
 }
-function Affcom(){
-    let accueil1 = document.getElementById("Section-Accueil-1");
-    let accueil2 = document.getElementById("Section-Accueil-2");
-    let APropos = document.getElementById("Section-A-Propos");
-    let CompGen = document.getElementById("Section-Compétence-Ensemble");
-    let Comp1 = document.querySelector(".Div-Comp-01");
-    let Comp2 = document.querySelector(".Div-Comp-02");
-    let Proj = document.getElementById("Section-Project");
-    let ProjGen1 = document.getElementById("Div-Project-Gen-1");
-    accueil1.style.display = "none";
-    accueil2.style.display = "none";
-    APropos.style.display = "none";
-    CompGen.style.display = "flex";
-    Comp1.style.display = "flex";
-    Comp2.style.display = "flex";
-    Proj.style.display = "none";
-    ProjGen1.style.display = "none";
+function Affcom() {
+  let accueil1 = document.getElementById("Section-Accueil-1");
+  let accueil2 = document.getElementById("Section-Accueil-2");
+  let APropos = document.getElementById("Section-A-Propos");
+  let CompGen = document.getElementById("Section-Compétence-Ensemble");
+  let Comp1 = document.querySelector(".Div-Comp-01");
+  let Comp2 = document.querySelector(".Div-Comp-02");
+  let Proj = document.getElementById("Section-Project");
+  let ProjGen1 = document.getElementById("Div-Project-Gen-1");
+  accueil1.style.display = "none";
+  accueil2.style.display = "none";
+  APropos.style.display = "none";
+  CompGen.style.display = "flex";
+  Comp1.style.display = "flex";
+  Comp2.style.display = "flex";
+  Proj.style.display = "none";
+  ProjGen1.style.display = "none";
 }
-function AffProj(){
-    let accueil1 = document.getElementById("Section-Accueil-1");
-    let accueil2 = document.getElementById("Section-Accueil-2");
-    let APropos = document.getElementById("Section-A-Propos");
-    let CompGen = document.getElementById("Section-Compétence-Ensemble");
-    let Comp1 = document.querySelector(".Div-Comp-01");
-    let Comp2 = document.querySelector(".Div-Comp-02");
-    let Proj = document.getElementById("Section-Project");
-    let ProjGen1 = document.getElementById("Div-Project-Gen-1");
-    accueil1.style.display = "none";
-    accueil2.style.display = "none";
-    APropos.style.display = "none";
-    CompGen.style.display = "none";
-    Comp1.style.display = "none";
-    Comp2.style.display = "none";
-    Proj.style.display = "flex";
-    ProjGen1.style.display = "flex";
+function AffProj() {
+  let accueil1 = document.getElementById("Section-Accueil-1");
+  let accueil2 = document.getElementById("Section-Accueil-2");
+  let APropos = document.getElementById("Section-A-Propos");
+  let CompGen = document.getElementById("Section-Compétence-Ensemble");
+  let Comp1 = document.querySelector(".Div-Comp-01");
+  let Comp2 = document.querySelector(".Div-Comp-02");
+  let Proj = document.getElementById("Section-Project");
+  let ProjGen1 = document.getElementById("Div-Project-Gen-1");
+  accueil1.style.display = "none";
+  accueil2.style.display = "none";
+  APropos.style.display = "none";
+  CompGen.style.display = "none";
+  Comp1.style.display = "none";
+  Comp2.style.display = "none";
+  Proj.style.display = "flex";
+  ProjGen1.style.display = "flex";
+}
+function P1() {
+  document.querySelector(".Div-Project-1").style.display = "block";
+  document.querySelector(".Div-Project-2").style.display = "none";
+}
+function P2() {
+  document.querySelector(".Div-Project-1").style.display = "none";
+  document.querySelector(".Div-Project-2").style.display = "block";
 }
